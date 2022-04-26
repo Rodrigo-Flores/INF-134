8789be933be6f9d0204f0dbb619591123a5c67c9
Rodrigo Flores <rodrigo.flores.12.07@gmail.com>
2022-04-26 12:49:24 -0400

    [ADD] example of creating lists (not ready)

---
+++ Practice/Personal/listas.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+typedef int tElemLista;
+
+class tLista {
+    private:
+        unsigned int maxSize;
+        unsigned int listSize;
+        unsigned int curr;
+        tElemLista * listArray;
+    public:
+        tLista();
+};
+
+int main () {
+
+    return 0;
+}
