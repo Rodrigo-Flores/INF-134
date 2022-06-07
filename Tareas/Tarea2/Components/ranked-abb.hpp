9be7d5eff06b0a3bacd59181d14c887559784346
Rodrigo Flores <rodrigo.flores.12.07@gmail.com>
2022-06-06 21:36:23 -0400

    [FIX] post-order recusion implementation

--- Tareas/Tarea2/Components/ranked-abb.hpp
+++ Tareas/Tarea2/Components/ranked-abb.hpp
@@ -17,40 +17,75 @@ private:
     unsigned int nElems;
 
 public:
+    /*****
+    * ABB ABB
+    ******
+    * Resumen Función
+    * Constructor de la clase ABB
+    ******
+    * Input:
+    * No recibe ningún parámetro
+    * .......
+    ******
+    * Returns:
+    * ABB, retirna el objeto en si, esto permite crear el objeto y usarlo en el resto de los métodos
+    *****/
     ABB()
     {
         raiz = new tipoNodoArbolBin;
         nElems = 0;
     }
-    void recursive_insert(tipoElementoArbol e, tipoNodoArbolBin *nodo)
+    /*****
+    * void recursive_insert
+    ******
+    * Resumen Función
+    * Inserta una elemento en el ABB de forma recursiva
+    ******
+    * Input:
+    * tipoElementoArbol elemento: elemento a insertar
+    * tipoNodoArbolBin *nodo: en un principio es la raiz, pero luego será cada nodo del árbol por la recursividad
+    * .......
+    ******
+    * Returns:
+    * No retorna ningún valor
+    *****/
+    void recursive_insert(tipoElementoArbol elemento, tipoNodoArbolBin *nodo)
     {
-        if (e > nodo->info)
+        if (elemento > nodo->info)
         {
             if (nodo->der == int(NULL))
             {
                 nodo->der = new tipoNodoArbolBin;
-                nodo->der->info = e;
+                nodo->der->info = elemento;
                 nElems++;
             }
-            else
-            {
-                recursive_insert(e, nodo->der);
-            }
+            else { recursive_insert(elemento, nodo->der); }
         }
-        else if (e < raiz->info)
+        else if (elemento < raiz->info)
         {
             if (nodo->izq == int(NULL))
             {
                 nodo->izq = new tipoNodoArbolBin;
-                nodo->izq->info = e;
+                nodo->izq->info = elemento;
                 nElems++;
             }
-            else
-            {
-                recursive_insert(e, nodo->izq);
-            }
+            else { recursive_insert(elemento, nodo->izq); }
         }
     }
+    /*****
+    * void insert
+    ******
+    * Resumen Función
+    * Es la función que se usa para llamar a la versión recursiva de insert
+    ******
+    * Input:
+    * tipoElementoArbol e: elemento a insertar
+    * tipoNodoArbolBin *nodo: en un principio es la raiz, pero luego será cada nodo del árbol por la recursividad
+    * .......
+    ******
+    * Returns:
+    * No retorna ningún valor
+    *****/
     void insert(tipoElementoArbol elemento)
     {
         if (raiz->info == int(NULL))
@@ -58,79 +93,159 @@ public:
             raiz->info = elemento;
             nElems++;
         }
-        else
-        {
-            recursive_insert(elemento, raiz);
-        }
+        else { recursive_insert(elemento, raiz); }
     }
+    /*****
+    * void recursive_print
+    ******
+    * Resumen Función
+    * Imprime el árbol de forma recursiva en in-order
+    ******
+    * Input:
+    * tipoNodoArbolBin *nodo: en un principio es la raiz, pero luego será cada nodo del árbol por la recursividad
+    * .......
+    ******
+    * Returns:
+    * No retorna ningún valor
+    *****/
     void recursive_print(tipoNodoArbolBin *nodo)
     {
         if (nodo->izq != int(NULL))
-        {
-            recursive_print(nodo->izq);
-        }
+        { recursive_print(nodo->izq); }
         cout << nodo->info << " ";
-        if (nodo->der != int(NULL))
-        {
-            recursive_print(nodo->der);
-        }
+        if (nodo->der != int(NULL)) { recursive_print(nodo->der); }
     }
+    /*****
+    * void print
+    ******
+    * Resumen Función
+    * es la función que se usa para llamar a la versión recursiva de print
+    ******
+    * Input:
+    * no recibe parametros
+    * .......
+    ******
+    * Returns:
+    * no retorna ningún valor
+    *****/
     void print()
     {
-        if (raiz->info == int(NULL))
-        {
-            cout << "El arbol esta vacio" << endl;
-        }
+        if (raiz->info == int(NULL)) { cout << "El arbol esta vacio" << endl; }
         else
         {
             cout << raiz->info << "\n";
             recursive_print(raiz);
         }
     }
-    bool find(tipoElementoArbol e)
+    /*****
+    * bool find
+    ******
+    * Resumen Función
+    * recorre el árbol a través de un ciclo while, para buscar el elemento que se le pasa por parámetro
+    ******
+    * Input:
+    * tipoElementoArbol elemento: elemento a buscar
+    * .......
+    ******
+    * Returns:
+    * true: si el elemento se encuentra en el árbol
+    * false: si el elemento no se encuentra en el árbol
+    *****/
+    bool find(tipoElementoArbol elemento)
     {
         tipoNodoArbolBin *nodo = raiz;
         while (nodo != int(NULL))
         {
-            if (e == nodo->info)
-            {
-                return true;
-            }
-            else if (e > nodo->info)
-            {
-                nodo = nodo->der;
-            }
-            else
-            {
-                nodo = nodo->izq;
-            }
+            if (elemento == nodo->info) { return true; }
+            else if (elemento > nodo->info) { nodo = nodo->der; }
+            else { nodo = nodo->izq; }
         }
         return false;
     }
-    int recursive_rank(tipoElementoArbol e, tipoNodoArbolBin *nodo) {
-        // search for the number of elements less than e in the tree
-        if (nodo == int(NULL)) {
-            return 0;
-        }
-        if (e < nodo->info) {
-            return recursive_rank(e, nodo->izq);
-        }
-        if (e >= nodo->info) {
-            return 1 + recursive_rank(e, nodo->der);
-        }
-        return 0;
+    /*****
+    * int recursive_rank
+    ******
+    * Resumen Función
+    * busca la cantidad de elementos menores o iguales que el elemento que se le pasa por parámetro
+    ******
+    * Input:
+    * tipoElementoArbol elemento: elemento a buscar
+    * tipoNodoArbolBin *nodo: en un principio es la raiz, pero luego será cada nodo del árbol por la recursividad
+    * int &count: contador que se incrementa cada vez que se encuentra un elemento menor o igual que el elemento que se le pasa por parámetro
+    * .......
+    ******
+    * Returns:
+    * count: retorna la cantidad de elementos menores o iguales que el elemento que se le pasa por parámetro
+    *****/
+    int recursive_rank(tipoElementoArbol elemento, tipoNodoArbolBin *nodo, int &count) {
+        if (nodo->izq != int(NULL)) { recursive_rank(elemento, nodo->izq, count); }
+        if (nodo->der != int(NULL)) { recursive_rank(elemento, nodo->der, count); }
+        if (elemento >= nodo->info) { count++; }
+        return count;
     }
-    int rank(tipoElementoArbol e)
+    /*****
+    * int rank
+    ******
+    * Resumen Función
+    * se usa para llamar a la versión recursiva de rank
+    ******
+    * Input:
+    * tipoElementoArbol elemento: elemento a buscar
+    * .......
+    ******
+    * Returns:
+    * recursive_rank(elemento, raiz, count): retorna el resultado de la recursividad de buscar los elementos menores o iguales al elemento dado
+    *****/
+    int rank(tipoElementoArbol elemento)
     {   
-        return recursive_rank(e, raiz);   
+        int count = 0;
+        return recursive_rank(elemento, raiz, count);   
     }
-    void eraseABB(tipoNodoArbolBin *nodo)
+    /*****
+    * void erase_abb
+    ******
+    * Resumen Función
+    * es el destructor de la clase
+    ******
+    * Input:
+    * tipoNodoArbolBin *nodo: la raiz del árbol, por la cual se recorre para borrar en post-order
+    * .......
+    ******
+    * Returns:
+    * recursive_rank(elemento, raiz, count): retorna el resultado de la recursividad de buscar los elementos menores o iguales al elemento dado
+    *****/
+    void erase_abb(tipoNodoArbolBin *nodo)
     {
-        if (nodo->izq != int(NULL))
-            eraseABB(nodo->izq);
-        if (nodo->der != int(NULL))
-            eraseABB(nodo->der);
+        if (nodo->izq != int(NULL)) { erase_abb(nodo->izq); }
+        if (nodo->der != int(NULL)) { erase_abb(nodo->der); }
         delete nodo;
     }
+    /*****
+    * tipoNodoArbolBin *get_raiz
+    ******
+    * Resumen Función
+    * getter de la raiz
+    ******
+    * Input:
+    * no recibe parametros
+    * .......
+    ******
+    * Returns:
+    * raiz: retorna la raiz del árbol
+    *****/
     tipoNodoArbolBin *get_raiz() { return raiz; }
+    /*****
+    * unsigned int get_nElems
+    ******
+    * Resumen Función
+    * getter de la cantidad de elementos del árbol
+    ******
+    * Input:
+    * no recibe parametros
+    * .......
+    ******
+    * Returns:
+    * raiz: retorna la raiz del árbol
+    *****/
+    unsigned int get_nElems() { return nElems; }
 };
