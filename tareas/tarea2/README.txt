0c51df7658c19b2e5e382e0a5c519df97b6fcd4a
Rodrigo-Flores <rodrigo.flores.12.07@gmail.com>
2022-08-23 23:25:30 -0400

    [UPT] gross merge from dev to main

---
+++ tareas/tarea2/README.txt
@@ -0,0 +1,12 @@
+Recordatorio:
+    1.- Hay que cambiar el nombre del archivo de lectura para TDA Lista dentro de p1.cpp
+    2.- Se puede cambiar la ruta de destino del ouput del TDA Lista en export_list
+
+Recomendaciones al compilar y probar:
+    1.- Para probar TDA lista: editar y correr desde p1.cpp
+    2.- Para probar TDA ABB: editar y correr desde p2.cpp
+
+Formas de compilación utilizada:
+    1.- Para todos las pruebas de la tareas se usó "g++ nombre.cpp -o program.out -Wall". Esto también se puede ver automatizado en un archivo bash adjunto.
+    2.- p1.cpp contiene la implementación del TDA lista desde Components/list.hpp
+    3.- p2.cpp contiene la implementación del TDA ABB desde Components/rank-abb.hpp
--- tareas/tarea2/p2.cpp
+++ tareas/tarea2/p2.cpp
@@ -1,98 +1,23 @@
 #include <iostream>
+#include "Components/ranked-abb.hpp"
 using namespace std;
-
-typedef int tipoElementoArbol;
-
-struct tipoNodoArbolBin
-{
-    tipoElementoArbol info = NULL;
-    tipoNodoArbolBin *izq = NULL;
-    tipoNodoArbolBin *der = NULL;
-};
-
-class ABB
-{
-private:
-    tipoNodoArbolBin *raiz;
-    unsigned int nElems;
-
-public:
-    ABB()
-    {
-        raiz = new tipoNodoArbolBin;
-        nElems = 0;
-    }
-    void recursive_insert(tipoElementoArbol e, tipoNodoArbolBin *nodo) {
-        if (e > nodo->info) {
-            if (nodo->der == NULL) {
-                nodo->der = new tipoNodoArbolBin;
-                nodo->der->info = e;
-                nElems++;
-            }
-            else {
-                recursive_insert(e, nodo->der);
-            }
-        } else if (e < raiz->info) {
-            if (nodo->izq == NULL) {
-                nodo->izq = new tipoNodoArbolBin;
-                nodo->izq->info = e;
-                nElems++;
-            }
-            else {
-                recursive_insert(e, nodo->izq);
-            }
-        } else {
-            return;
-        }
-    }
-    void insert(tipoElementoArbol elemento) {
-        if (raiz->info == NULL) {
-            raiz = new tipoNodoArbolBin;
-            raiz->info = elemento;
-            nElems++;
-        } else {
-            recursive_insert(elemento, raiz);
-        }
-    }
-    void recursive_print(tipoNodoArbolBin *nodo) {
-        if (nodo->izq != NULL) {
-            recursive_print(nodo->izq);
-        }
-        cout << nodo->info << " ";
-        if (nodo->der != NULL) {
-            recursive_print(nodo->der);
-        }
-    }
-    void print() {
-        if (raiz->info == NULL) {
-            cout << "El arbol esta vacio" << endl;
-        } else {
-            recursive_print(raiz);
-        }
-    }
-    void eraseABB(tipoNodoArbolBin *nodo)
-    {
-        if (nodo->izq != NULL)
-            eraseABB(nodo->izq);
-        if (nodo->der != NULL)
-            eraseABB(nodo->der);
-        delete nodo;
-    }
-    tipoNodoArbolBin *get_raiz() { return raiz; }
-};
 
 int main()
 {
+    /* EJEMPLO */
+
+    /*
     ABB arbol;
-    arbol.insert(5);
-    arbol.insert(3);
-    arbol.insert(7);
-    arbol.insert(2);
-    arbol.insert(4);
-    arbol.insert(6);
+    
+    for (int i = 0; i < 10000; i++)
+    {
+        arbol.insert(i);
+    }
 
-    arbol.print();
+    cout << arbol.rank(1000) << endl;
 
-    arbol.eraseABB(arbol.get_raiz());
+    arbol.erase_abb(arbol.get_raiz());
+    */
+   
     return 0;
 }
